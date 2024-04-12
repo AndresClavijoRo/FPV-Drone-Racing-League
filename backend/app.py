26143d3edca7fe6d87696a5d59cf8354d51542e1
fclopez4 <fc.lopez@uniandes.edu.co>
2024-04-12 01:19:16 -0500

    feat: se genera el archivo docker compose inicial feat: se genera el archivo dockerfile inicial

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
+import os
 from build_flask_app import create_flask_app
 from models import db
 
 flask_app = create_flask_app()
 app = create_flask_app()
-if app.debug:
+
+if os.getenv("FLASK_ENV") == "testing":
+    app.testing = True
+
+if app.debug or app.testing:
     db.create_all()
