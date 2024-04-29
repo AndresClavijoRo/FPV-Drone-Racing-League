14bf70a17c9df7fa11b32f509ba1d805552adf63
andresalarcon <w.alarconc@uniandes.edu.co>
2024-04-28 19:32:50 -0500

    se borra nginx

---
+++ Gunicorn/backend/app.py
@@ -0,0 +1,20 @@
+import click
+
+from build_flask_app import create_flask_app
+from models import db
+#from load_testing import load_test
+
+app = None
+
+flask_app = create_flask_app()
+
+app = create_flask_app()
+db.init_app(app)
+db.create_all()
+
+
+#@app.cli.command("load-test")
+#@click.option("--total-tasks", default=50, help="Total tasks to create")
+#@click.option("--total-minutes", default=10, help="Total tasks to create")
+#def load_test_tasks(total_tasks, total_minutes):
+#   load_test(app, total_tasks, total_minutes)
